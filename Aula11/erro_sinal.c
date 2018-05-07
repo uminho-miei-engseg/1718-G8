eed7be1a5d65a0b0ab782abb0ebc18d8a5700412
Valéria <32262816+basementscene@users.noreply.github.com>
2018-05-07 23:52:21 +0100

    ficheiros aula11

---
+++ Aula11/erro_sinal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const int MAX_SIZE = 2048;
+
+
+void vulneravel (char *origem, size_t tamanho) {
+        int tamanho_real;
+        char *destino;
+        if (tamanho > 1) {
+                tamanho_real = tamanho - 1; // Não copiar \0 de origem para destino
+                if (tamanho_real < MAX_SIZE) {
+						printf("%d\n", tamanho_real);
+                        destino = (char *) malloc(tamanho_real);
+                        memcpy(destino, origem, tamanho_real);
+                }
+        }
+}
+
+int main() {
+	
+	vulneravel("Teste", -5);
+}
